025a0deaef7f956c534621fe59ea75293f3cef87
nox-interior <mariam.arroyo@gmail.com>
2025-04-02 10:22:29 +0200

    minishell header

--- tokenizer/minishell.h
+++ tokenizer/minishell.h
@@ -6,14 +6,35 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 09:55:21 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/04/02 09:58:38 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/04/02 10:21:23 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include "libft.h"
-#include "stdio.h"
+# include "libft.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "stdio.h"
+
+// Token codes
+typedef enum e_token
+{
+    T_STRING,           //ENTRE COMILLAS "" ''?
+    T_WORD,             //SIN COMILLAS
+    T_PIPE,
+    T_REDIR_IN,
+    T_REDIR_OUT,
+    T_HEREDOC,
+    T_APPEND,
+    T_PIPE,
+    T_QUOTE,
+    T_DOUBLE_QUOTE,
+    
+    
+}
 
 #endif
