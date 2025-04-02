9d6e31a042a0f8a5698642fa53351b7711376ac3
nox <mariam.arroyo@gmail.com>
2025-04-02 16:37:52 +0000

    funciones para crear nuevos tokens y añadirlos a una lista + aux para imprimir. Atención, memoria no liberada

--- tokenizer/minishell.h
+++ tokenizer/minishell.h
@@ -6,43 +6,45 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 09:55:21 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/04/02 13:23:50 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/04/02 16:34:20 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-//# include "libft.h"
+# include "../libft/inc/libft.h"
 //# include "ft_printf.h"
 //# include "get_next_line.h"
-# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h> // malloc
+# include <string.h> // para strdup
 # include <readline/history.h>
-# include "stdio.h"
+# include <readline/readline.h>
 
 // Token codes
-typedef enum    e_tok_type
+typedef enum e_tok_type
 {
-    T_NONE, //para evitar tener tokens sin tipo, control de errores
-    T_SPACES,
-    T_WORD,
-    T_VAR,
-    T_PIPE,
-    T_REDIR_IN,
-    T_REDIR_OUT,
-    T_HEREDOC,
-    T_APPEND
-}               t_tok_type;
+	T_NONE, // para evitar tener tokens sin tipo, control de errores
+	T_SPACES,
+	T_WORD,
+	T_STRING,
+	T_VAR,
+	T_PIPE,
+	T_REDIR_IN,
+	T_REDIR_OUT,
+	T_HEREDOC,
+	T_APPEND
+}					t_tok_type;
 
 // Token structure
-typedef struct      s_token
+typedef struct s_token
 {
-    t_tok_type      type;
-    char            *node;
-    struct s_token  *next;
-    struct s_token  *prev;
-}                   t_token;
-
+	t_tok_type		type;
+	char			*value;
+	struct s_token	*next;
+	struct s_token	*prev;
+}					t_token;
 
 //
 
--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -6,21 +6,103 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 13:22:57 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/04/02 13:55:06 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/04/02 16:34:54 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    ft_tokenizer(char *prompt)
+t_token	*ft_new_token(t_tok_type type, char *value)
 {
-    t_token *token;
-    int i;
-    
-    i = 0;
-    while (prompt[i])
-    {
-        if (prompt[i] == '<')
-            set_status(T_REDIR_IN); // o return, como lo metemos en la estructura??
-    }
+	t_token	*token;
+	
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->type = type;
+	token->value = value;
+	token->next = NULL;
+	token->prev = NULL;
+	return (token);
 }
+
+void	ft_add_token(t_token **head, t_token *new_token)
+{
+	t_token	*last_token;
+
+	if (!head || !new_token)
+		return ;
+	if (!*head)
+	{
+		*head = new_token;
+		return ;
+	}
+	last_token = *head;
+	while (last_token->next)
+		last_token = last_token->next;
+	last_token->next = new_token;
+	new_token->prev = last_token;
+}
+
+char	*ft_token_type_name(t_tok_type type)
+{
+	if (type == T_NONE)
+		return ("NONE");
+	else if (type == T_SPACES)
+		return ("SPACES");
+	else if (type == T_WORD)
+		return ("WORD");
+	else if (type == T_STRING)
+		return ("STRING");
+	else if (type == T_VAR)
+		return ("VAR");
+	else if (type == T_PIPE)
+		return ("PIPE");
+	else if (type == T_REDIR_IN)
+		return ("REDIR_IN");
+	else if (type == T_REDIR_OUT)
+		return ("REDIR_OUT");
+	else if (type == T_HEREDOC)
+		return ("HEREDOC");
+	else if (type == T_APPEND)
+		return ("APPEND");
+	return ("UNKNOWN");
+}
+
+void	ft_print_tokens(t_token *head)
+{
+	while (head)
+	{
+		printf("Token: [%-10s] -> \"%s\"\n",
+			ft_token_type_name(head->type), head->value);
+		head = head->next;
+	}
+}
+
+int	main(void)
+{
+	t_token	*tokens = NULL;
+
+	ft_add_token(&tokens, ft_new_token(T_WORD, ft_strdup("echo")));
+	ft_add_token(&tokens, ft_new_token(T_STRING, ft_strdup("hola mundo")));
+	ft_add_token(&tokens, ft_new_token(T_REDIR_OUT, ft_strdup(">")));
+	ft_add_token(&tokens, ft_new_token(T_WORD, ft_strdup("salida.txt")));
+
+	ft_print_tokens(tokens);
+	return (0);
+}
+
+// void	ft_tokenizer(char *prompt)
+// {
+// 	t_token *token_list;
+// 	int i;
+
+// 	i = 0;
+// 	token_list = NULL;
+// 	while (prompt[i])
+// 	{
+// 		if (prompt[i] == 32)
+// 			i++;
+// 		else if (prompt[i])
+// 	}
+// }
