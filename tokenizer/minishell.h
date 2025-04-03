26594d255129978db2b1b0a6e572d7f052d9d4a8
nox <mariam.arroyo@gmail.com>
2025-04-03 11:01:34 +0000

    Added functions for redirection tokenizing

--- tokenizer/minishell.h
+++ tokenizer/minishell.h
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 09:55:21 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/04/03 09:05:19 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/04/03 10:52:21 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 # include "../libft/inc/libft.h"
 //# include "ft_printf.h"
 //# include "get_next_line.h"
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h> // malloc
 # include <string.h> // para strdup
-# include <readline/history.h>
-# include <readline/readline.h>
 //# include <stdbool.h> // para bool
 
 // Token codes
@@ -36,7 +36,7 @@ typedef enum e_tok_type
 	T_REDIR_OUT,
 	T_HEREDOC,
 	T_APPEND
-}					t_tok_type;
+}				t_tok_type;
 
 // Token structure
 typedef struct s_token
@@ -46,5 +46,11 @@ typedef struct s_token
 	struct s_token	*next;
 	struct s_token	*prev;
 }					t_token;
+
+// 
+t_token	*ft_tokenizer(const char *prompt);
+int	main(void);
+void	ft_add_token(t_token **head, t_token *new_token);
+t_token	*ft_new_token(t_tok_type type, char *value);
 
 #endif
--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 13:22:57 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/04/03 09:18:06 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/04/03 11:00:27 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_token	*ft_new_token(t_tok_type type, char *value)
 {
 	t_token	*token;
-	
+
 	token = malloc(sizeof(t_token));
 	if (!token)
 		return (NULL);
@@ -50,13 +50,12 @@ void	ft_add_token(t_token **head, t_token *new_token)
 	new_token->prev = last_token;
 }
 
-char	*ft_token_type_name(t_tok_type type)
+static char	*ft_token_type_name(t_tok_type type)
 {
-	if (type == T_NONE) // No estoy segura de si este es necesario o no pero por 
-	//el momento lo incluyo, tendría más sentido para gestionar errores, 
-	//pero bueno, iremos viendo si esto sirve de algo o no.
+	if (type == T_NONE)
 		return (NULL);
 	else if (type == T_SPACES)
+		// Es necesario si vamos a saltar espacios? No creo
 		return ("SPACES");
 	else if (type == T_WORD)
 		return ("WORD");
@@ -74,62 +73,114 @@ char	*ft_token_type_name(t_tok_type type)
 		return ("HEREDOC");
 	else if (type == T_APPEND)
 		return ("APPEND");
-	return ("UNKNOWN"); // mismo rollo que NONE, no sé si tendrçia más sentido devolver un error.
+	return ("UNKNOWN");
+	// no sé si tendrçia más sentido devolver un error o quitarlo directamente porque ya maneja t_none un error
 }
 
 // Esta solo sirve para hacer pruebas, ni caso.
-void	ft_print_tokens(t_token *head)
+static void	ft_print_tokens(t_token *head)
 {
 	while (head)
 	{
-		printf("Token: [%-10s] -> \"%s\"\n",
-			ft_token_type_name(head->type), head->value);
+		printf("Token: [%-10s] -> \"%s\"\n", ft_token_type_name(head->type),
+			head->value);
 		head = head->next;
 	}
 }
 
-int	main(void)
-{
-	t_token	*tokens = NULL;
-
-	ft_add_token(&tokens, ft_new_token(T_WORD, ft_strdup("echo")));
-	ft_add_token(&tokens, ft_new_token(T_STRING, ft_strdup("hola mundo")));
-	ft_add_token(&tokens, ft_new_token(T_REDIR_OUT, ft_strdup(">")));
-	ft_add_token(&tokens, ft_new_token(T_WORD, ft_strdup("salida.txt")));
-
-	ft_print_tokens(tokens);
-	return (0);
-}
 // Funcion para espacios en blanco (se puede añadir a libft?)
-static int	ft_isspace(char c)
-{
-	if (c == 32 || (c >= 9 && c <= 13))
-		return (1);
-	return (0);
-}
+// static int	ft_isspace(char c)
+// {
+// 	if (c == 32 || (c >= 9 && c <= 13))
+// 		return (1);
+// 	return (0);
+// }
 
-// Funcion para saltar espacios en blanco
-static int ft_skip_spaces(char *prompt, int i)
-{
-	while (prompt[i] && ft_isspace(prompt[i]))
-		i++;
-	return (i);
-}
-// Funcion para caracteres especiales <, >, |, <<, >>
-static t_tok_type
+// // Funcion para saltar espacios en blanco
+// static int	ft_skip_spaces(const char *prompt, int i)
+// {
+// 	while (prompt[i] && ft_isspace(prompt[i]))
+// 		i++;
+// 	return (i);
+// }
+
+// Función para pipe
 // Funcion para comillas
 // Funcion para caracteres normales
 
-/*	*ft_tokenizer(char *prompt)
+// Funciones para caracteres especiales <, >, <<, >>
+static void	ft_redir(t_token **token_list, const char c)
 {
-	t_token *token_list;
-	int i;
+	t_token	*new_token;
+
+	if (c == '<')
+	{
+		new_token = ft_new_token(T_REDIR_IN, "<");
+		ft_add_token(token_list, new_token);
+		return ;
+	}
+	if (c == '>')
+	{
+		new_token = ft_new_token(T_REDIR_OUT, ">");
+		ft_add_token(token_list, new_token);
+		return ;
+	}
+}
+
+static void	ft_dub_redir(t_token **token_list, const char c)
+{
+	t_token	*new_token;
+
+	if (c == '<')
+	{
+		new_token = ft_new_token(T_HEREDOC, "<<");
+		ft_add_token(token_list, new_token);
+		return ;
+	}
+	if (c == '>')
+	{
+		new_token = ft_new_token(T_APPEND, ">>");
+		ft_add_token(token_list, new_token);
+		return ;
+	}
+}
+
+t_token	*ft_tokenizer(const char *prompt)
+{
+	t_token	*token_list;
+	int		i;
 
 	i = 0;
 	token_list = NULL;
 	while (prompt[i])
 	{
-		// casos
+		if (prompt[i] == '<' || prompt[i] == '>')
+		{
+			if (prompt[i] == prompt[i + 1])
+			{
+				ft_dub_redir(&token_list, prompt[i]); // TODO
+				i++;
+			}
+			else
+				ft_redir(&token_list, prompt[i]);
+		}
+		i++;
 	}
+	return (token_list);
 }
-*/
+
+int	main(void)
+{
+	t_token	*tokens;
+
+	tokens = NULL;
+	tokens = ft_tokenizer("<<<>>>");
+	/*ft_add_token(&tokens, ft_new_token(T_WORD, ft_strdup("echo")));
+	ft_add_token(&tokens, ft_new_token(T_STRING,
+			ft_strdup("hola mundo")));
+	ft_add_token(&tokens, ft_new_token(T_REDIR_OUT, ft_strdup(">")));
+	ft_add_token(&tokens, ft_new_token(T_WORD,
+			ft_strdup("salida.txt")));*/
+	ft_print_tokens(tokens);
+	return (0);
+}
