b66ff9ee5cd60fa1851015fe60a02400cf53aab6
nox <mariam.arroyo@gmail.com>
2025-04-03 08:15:49 +0000

    comments for clarity

--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 13:22:57 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/04/02 16:34:54 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/04/03 08:14:42 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,9 @@ void	ft_add_token(t_token **head, t_token *new_token)
 
 char	*ft_token_type_name(t_tok_type type)
 {
-	if (type == T_NONE)
+	if (type == T_NONE) // No estoy segura de si este es necesario o no pero por 
+	//el momento lo incluyo, tendría más sentido para gestionar errores, 
+	//pero bueno, iremos viendo si esto sirve de algo o no.
 		return ("NONE");
 	else if (type == T_SPACES)
 		return ("SPACES");
@@ -66,7 +68,7 @@ char	*ft_token_type_name(t_tok_type type)
 		return ("HEREDOC");
 	else if (type == T_APPEND)
 		return ("APPEND");
-	return ("UNKNOWN");
+	return ("UNKNOWN"); // mismo rollo que NONE, no sé si tendrçia más sentido devolver un error.
 }
 
 void	ft_print_tokens(t_token *head)
@@ -92,17 +94,17 @@ int	main(void)
 	return (0);
 }
 
-// void	ft_tokenizer(char *prompt)
-// {
-// 	t_token *token_list;
-// 	int i;
+void	ft_tokenizer(char *prompt)
+{
+	t_token *token_list;
+	int i;
 
-// 	i = 0;
-// 	token_list = NULL;
-// 	while (prompt[i])
-// 	{
-// 		if (prompt[i] == 32)
-// 			i++;
-// 		else if (prompt[i])
-// 	}
-// }
+	i = 0;
+	token_list = NULL;
+	while (prompt[i])
+	{
+		if (prompt[i] == 32)
+			i++;
+		else if (prompt[i])
+	}
+}
