b28c10441ca6197a1a9df6936024ebc7e4c6a263
nox <mariam.arroyo@gmail.com>
2025-04-04 11:54:47 +0000

    tokenizer OK

--- tokenizer/minishell.h
+++ tokenizer/minishell.h
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 09:55:21 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/04/03 10:52:21 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/04/04 11:30:18 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 //# include "get_next_line.h"
 # include <readline/history.h>
 # include <readline/readline.h>
+# include <stdbool.h> // para bool
 # include <stdio.h>
 # include <stdlib.h> // malloc
 # include <string.h> // para strdup
-//# include <stdbool.h> // para bool
 
 // Token codes
 typedef enum e_tok_type
@@ -29,14 +29,15 @@ typedef enum e_tok_type
 	T_NONE, // para evitar tener tokens sin tipo, control de errores
 	T_SPACES,
 	T_WORD,
-	T_STRING,
+	T_S_QUOTE,
+	T_D_QUOTE,
 	T_VAR,
 	T_PIPE,
 	T_REDIR_IN,
 	T_REDIR_OUT,
 	T_HEREDOC,
 	T_APPEND
-}				t_tok_type;
+}					t_tok_type;
 
 // Token structure
 typedef struct s_token
@@ -47,10 +48,11 @@ typedef struct s_token
 	struct s_token	*prev;
 }					t_token;
 
-// 
-t_token	*ft_tokenizer(const char *prompt);
-int	main(void);
-void	ft_add_token(t_token **head, t_token *new_token);
-t_token	*ft_new_token(t_tok_type type, char *value);
+//
+t_token				*ft_tokenizer(const char *prompt);
+void				ft_add_token(t_token **head, t_token *new_token);
+t_token				*ft_new_token(t_tok_type type, char *value);
+void				ft_free_token_list(t_token **tokens);
+int					ft_word_token(t_token **tokens, const char *prompt, int i);
 
 #endif
--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 13:22:57 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/04/03 12:23:18 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/04/04 11:52:19 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,17 @@ t_token	*ft_new_token(t_tok_type type, char *value)
 {
 	t_token	*token;
 
+	if (type == T_NONE)
+	{
+		free(value);
+		return (NULL);
+	}
 	token = malloc(sizeof(t_token));
 	if (!token)
+	{
+		free(value);
 		return (NULL);
+	}
 	token->type = type;
 	token->value = value;
 	token->next = NULL;
@@ -32,12 +40,6 @@ void	ft_add_token(t_token **head, t_token *new_token)
 
 	if (!head || !new_token)
 		return ;
-	if (new_token->type == T_NONE)
-	{
-		free(new_token->value);
-		free(new_token);
-		return ;
-	}
 	if (!*head)
 	{
 		*head = new_token;
@@ -50,17 +52,41 @@ void	ft_add_token(t_token **head, t_token *new_token)
 	new_token->prev = last_token;
 }
 
+// Liberar lista  de tokens
+void	ft_free_token_list(t_token **tokens)
+{
+	t_token	*tmp;
+
+	if (!tokens || !*tokens)
+		return ;
+	while (*tokens)
+	{
+		tmp = (*tokens)->next;
+		if ((*tokens)->value)
+			free((*tokens)->value);
+		free(*tokens);
+		*tokens = tmp;
+	}
+	*tokens = NULL;
+}
+
+/*
+Advertencia menor:
+	•	Si se usa esta función solo en ft_print_tokens(), está bien.
+	•	Si alguna se usa ft_token_type_name() en una lógica más crítica,
+			cuidado con el "UNKNOWN" y el NULL.
+	Podría producir un segfault en printf("%s", NULL).*/
+
 static char	*ft_token_type_name(t_tok_type type)
 {
 	if (type == T_NONE)
 		return (NULL);
-	else if (type == T_SPACES)
-		// Es necesario si vamos a saltar espacios? No creo
-		return ("SPACES");
 	else if (type == T_WORD)
 		return ("WORD");
-	else if (type == T_STRING)
-		return ("STRING");
+	else if (type == T_S_QUOTE)
+		return ("S_QUOTE");
+	else if (type == T_D_QUOTE)
+		return ("D_QUOTE");
 	else if (type == T_VAR)
 		return ("VAR");
 	else if (type == T_PIPE)
@@ -74,7 +100,6 @@ static char	*ft_token_type_name(t_tok_type type)
 	else if (type == T_APPEND)
 		return ("APPEND");
 	return ("UNKNOWN");
-	// no sé si tendrçia más sentido devolver un error o quitarlo directamente porque ya maneja t_none un error
 }
 
 // Esta solo sirve para hacer pruebas, ni caso.
@@ -89,12 +114,12 @@ static void	ft_print_tokens(t_token *head)
 }
 
 // Funcion para espacios en blanco (se puede añadir a libft?)
-// static int	ft_isspace(char c)
-// {
-// 	if (c == 32 || (c >= 9 && c <= 13))
-// 		return (1);
-// 	return (0);
-// }
+static int	ft_isspace(char c)
+{
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
+}
 
 // // Funcion para saltar espacios en blanco
 // static int	ft_skip_spaces(const char *prompt, int i)
@@ -104,42 +129,32 @@ static void	ft_print_tokens(t_token *head)
 // 	return (i);
 // }
 
-// Función para variables
-// Función para pipe
-
 // Funcion para comillas
-// static void	ft_quotes(t_token **token_list, const char *prompt, int *i)
-
-#include "minishell.h"
-
-// Detecta una cadena entre comillas y añade un token STRING.
-// Devuelve el nuevo valor de i tras la comilla de cierre,
-// o -1 si no se encuentra comilla de cierre.
-int	ft_handle_quotes(t_token **tokens, const char *prompt, int i)
+int	ft_quotes_token(t_token **tokens, const char *prompt, int i)
 {
 	int		start;
 	char	quote;
 	char	*value;
 
-	quote = prompt[i]; // puede ser ' o "
-	i++;               // saltar la comilla de apertura
+	quote = prompt[i];
+	i++;
 	start = i;
 	while (prompt[i] && prompt[i] != quote)
 		i++;
 	if (prompt[i] == '\0')
 	{
-		ft_putstr_fd("minishell: error: comilla sin cerrar\n", 2);
+		ft_putstr_fd("minishell: error: non-closed quotes\n", 2);
 		return (-1);
 	}
-	// Extraer el contenido entre comillas
 	value = ft_substr(prompt, start, i - start);
 	if (!value)
 		return (-1);
-	ft_add_token(tokens, ft_new_token(T_STRING, value));
-	return (i + 1); // avanzar tras la comilla de cierre
+	if (quote == '\'')
+		ft_add_token(tokens, ft_new_token(T_S_QUOTE, value));
+	else if (quote == '\"')
+		ft_add_token(tokens, ft_new_token(T_D_QUOTE, value));
+	return (i + 1);
 }
-
-// Funcion para caracteres normales
 
 // Funciones para caracteres especiales <, >, <<, >>
 static void	ft_redir(t_token **token_list, const char c)
@@ -148,17 +163,34 @@ static void	ft_redir(t_token **token_list, const char c)
 
 	if (c == '<')
 	{
-		new_token = ft_new_token(T_REDIR_IN, "<");
+		new_token = ft_new_token(T_REDIR_IN, ft_strdup("<"));
+		if (!new_token)
+			return ;
 		ft_add_token(token_list, new_token);
 		return ;
 	}
 	if (c == '>')
 	{
-		new_token = ft_new_token(T_REDIR_OUT, ">");
+		new_token = ft_new_token(T_REDIR_OUT, ft_strdup(">"));
+		if (!new_token)
+			return ;
 		ft_add_token(token_list, new_token);
 		return ;
 	}
 }
+
+// static void	ft_redir(t_token **token_list, const char c)
+// {
+// 	t_token	*new_token;
+
+// 	if (c == '<')
+// 		new_token = ft_new_token(T_REDIR_IN, ft_strdup("<"));
+// 	else
+// 		new_token = ft_new_token(T_REDIR_OUT, ft_strdup(">"));
+// 	if (!new_token)
+// 		return ;
+// 	ft_add_token(token_list, new_token);
+// }
 
 static void	ft_dub_redir(t_token **token_list, const char c)
 {
@@ -166,49 +198,267 @@ static void	ft_dub_redir(t_token **token_list, const char c)
 
 	if (c == '<')
 	{
-		new_token = ft_new_token(T_HEREDOC, "<<");
+		new_token = ft_new_token(T_HEREDOC, ft_strdup("<<"));
+		if (!new_token)
+			return ;
 		ft_add_token(token_list, new_token);
 		return ;
 	}
 	if (c == '>')
 	{
-		new_token = ft_new_token(T_APPEND, ">>");
+		new_token = ft_new_token(T_APPEND, ft_strdup(">>"));
+		if (!new_token)
+			return ;
 		ft_add_token(token_list, new_token);
 		return ;
 	}
 }
+// static void	ft_dub_redir(t_token **token_list, const char c)
+// {
+// 	t_token	*new_token;
 
+// 	if (c == '<')
+// 		new_token = ft_new_token(T_HEREDOC, ft_strdup("<<"));
+// 	else
+// 		new_token = ft_new_token(T_APPEND, ft_strdup(">>"));
+// 	if (!new_token)
+// 		return ;
+// 	ft_add_token(token_list, new_token);
+// }
+
+// Función para pipe
 static void	ft_pipe_token(t_token **token_list)
 {
-	new_token = ft_new_token(T_PIPE, "|");
+	t_token	*new_token;
+
+	new_token = ft_new_token(T_PIPE, ft_strdup("|"));
+	if (!new_token)
+		return ;
 	ft_add_token(token_list, new_token);
 }
+
+// Función para variables
+// static int	ft_var_token(t_token **token_list, const char *prompt, int i)
+// {
+// 	int		start;
+// 	t_token	*new_token;// NULL?
+
+// 	start = i + 1;// Empieza en el siguiente al $
+// 	i += 1;
+// 	if (prompt[i] == '?')
+// 	{
+// 		new_token = ft_new_token(T_VAR, ft_strdup("?"));
+// 		ft_add_token(token_list, new_token);
+// 		return (i);
+// 	}
+// 	while (prompt[i])
+// 	{
+// 		// if (!ft_isalpha(prompt[i]) || !ft_isdigit(prompt[i])
+// 		// 	|| !prompt[i] == '_')
+// 		if (!(ft_isalpha(prompt[i]) || ft_isdigit(prompt[i])
+//|| prompt[i] == '_'))
+// 			break ;
+// 		i++;
+// 	}
+// 	new_token = ft_new_token(T_VAR, ft_strdup(ft_substr(prompt, start, i
+// 					- start)));
+// 	ft_add_token(token_list, new_token);
+// 	return (i - 1);
+// }
+
+// static int	ft_var_token(t_token **token_list, const char *prompt, int i)
+// {
+// 	int		start;
+// 	char	*value;
+// 	t_token	*new_token;
+
+// 	start = i + 1;
+// 	i++;
+// 	if (prompt[i] == '?')
+// 	{
+// 		new_token = ft_new_token(T_VAR, ft_strdup("?"));
+// 		if (!new_token)
+// 			return (-1);
+// 		ft_add_token(token_list, new_token);
+// 		return (i);
+// 	}
+// 	while (prompt[i])
+// 	{
+// 		if (!ft_isalpha(prompt[i]) && !ft_isdigit(prompt[i]) && prompt[i] != '_'
+// 			&& ft_isspace(prompt[i]))
+// 			break ;
+// 		i++;
+// 	}
+// 	if (i - start <= 0)
+// 		return (0);
+// 	value = ft_substr(prompt, start, i - start);
+// 	if (!value)
+// 		return (-1);
+// 	new_token = ft_new_token(T_VAR, value);
+// 	if (!new_token)
+// 	{
+// 		free(value);
+// 		return (-1);
+// 	}
+// 	ft_add_token(token_list, new_token);
+// 	return (i - 1);
+// }
 
 static int	ft_var_token(t_token **token_list, const char *prompt, int i)
 {
-	int	start;
+	int		start;
+	char	*value;
+	t_token	*new_token;
 
-	t_token *new_token; // NULL?
-	start = i + 1;      // Empieza en el siguiente al $
-	i += 1;
+	start = i + 1;
+	i++;
+	// ⚠️ Si después del $ viene un espacio o fin de línea, NO es una variable
+	if (prompt[i] == '\0' || ft_isspace(prompt[i]))
+	{
+		// // Lo tratamos como T_WORD en lugar de T_VAR
+		// value = ft_substr(prompt, start - 1, 1); // solo "$"
+		// if (!value)
+		// 	return (-1);
+		// new_token = ft_new_token(T_WORD, value);
+		// if (!new_token)
+		// {
+		// 	free(value);
+		// 	return (-1);
+		// }
+		// ft_add_token(token_list, new_token);
+		// return (i - 1); // volvemos al espacio
+		return (ft_word_token(token_list, prompt, i - 1));
+	}
 	if (prompt[i] == '?')
 	{
-		new_token = ft_new_token(T_VAR, "?");
+		new_token = ft_new_token(T_VAR, ft_strdup("?"));
+		if (!new_token)
+			return (-1);
 		ft_add_token(token_list, new_token);
 		return (i);
 	}
-	while (prompt[i])
-	{
-		if (!ft_isalpha(prompt[i]) || !ft_isdigit(prompt[i])
-			|| !prompt[i] == '_')
-			break ;
+	while (prompt[i] && (ft_isalpha(prompt[i]) || ft_isdigit(prompt[i])
+			|| prompt[i] == '_'))
 		i++;
+	if (prompt[i] == '$')
+		i++;
+	if (i - start <= 0)
+		return (-1);
+	value = ft_substr(prompt, start, i - start);
+	if (!value)
+		return (-1);
+	new_token = ft_new_token(T_VAR, value);
+	if (!new_token)
+	{
+		free(value);
+		return (-1);
 	}
-	new_token = ft_new_token(T_VAR, ft_strdup(ft_substr(prompt, start, i
-					- start)));
 	ft_add_token(token_list, new_token);
 	return (i - 1);
 }
+
+// static int	ft_var_token(t_token **token_list, const char *prompt, int i)
+// {
+// 	int	start;
+
+// 	t_token *new_token; // NULL?
+// 	start = i + 1;      // Empieza en el siguiente al $
+// 	i += 1;
+// 	if (prompt[i] == '?')
+// 	{
+// 		new_token = ft_new_token(T_VAR, "?");
+// 		ft_add_token(token_list, new_token);
+// 		return (i);
+// 	}
+// 	while (prompt[i])
+// 	{
+// 		if (!ft_isalpha(prompt[i]) || !ft_isdigit(prompt[i])
+// 			|| !(prompt[i] == '_'))
+// 			break ;
+// 		i++;
+// 	}
+// 	new_token = ft_new_token(T_VAR, ft_strdup(ft_substr(prompt, start, i
+// 					- start)));
+// 	ft_add_token(token_list, new_token);
+// 	return (i - 1);
+// }
+
+// Funcion para caracteres normales
+bool	ft_is_word_char(char c)
+{
+	if (c == '\0')
+		return (false);
+	if (ft_isspace(c))
+		return (false);
+	if (c == '|' || c == '<' || c == '>' || c == '\'' || c == '\"')
+		return (false);
+	return (true);
+}
+
+int	ft_word_token(t_token **tokens, const char *prompt, int i)
+{
+	int		start;
+	int		len;
+	char	*value;
+	t_token	*new_token;
+
+	start = i;
+	while (prompt[i] && ft_is_word_char(prompt[i]))
+		i++;
+	len = i - start;
+	value = ft_substr(prompt, start, len);
+	if (!value)
+		return (-1);
+	new_token = ft_new_token(T_WORD, value);
+	if (!new_token)
+	{
+		free(value);
+		return (-1);
+	}
+	ft_add_token(tokens, new_token);
+	return (i);
+}
+
+// Tokenizer
+// t_token	*ft_tokenizer(const char *prompt)
+// {
+// 	t_token	*token_list;
+// 	int		i;
+
+// 	i = 0;
+// 	token_list = NULL;
+// 	while (prompt[i])
+// 	{
+// 		if (prompt[i] == '<' || prompt[i] == '>')
+// 		{
+// 			if (prompt[i] == prompt[i + 1])
+// 			{
+// 				ft_dub_redir(&token_list, prompt[i]);
+// 				i++;
+// 			}
+// 			else
+// 				ft_redir(&token_list, prompt[i]);
+// 		}
+// 		else if (prompt[i] == '|')
+// 			ft_pipe_token(&token_list);
+// 		else if (prompt[i] == '\'' || prompt[i] == '\"')
+// 			i = ft_quotes_token(&token_list, prompt, i);
+// 		else if (prompt[i] == '$')
+// 			i = ft_var_token(&token_list, prompt, i);
+// 		else
+// 			// caract normales
+// 		i++;
+// 	}
+// 	return (token_list);
+// }
+
+/*
+cada bloque (<, >, |, ', ", $, etc.) consume el carácter actual y actualiza i,
+por lo que no queremos que el bucle vuelva a hacer i++ otra vez después.
+Así evitamos:
+	•	Duplicar avances de i
+	•	Evaluar innecesariamente otros if después de haber procesado el carácter
+*/
 
 t_token	*ft_tokenizer(const char *prompt)
 {
@@ -219,6 +469,10 @@ t_token	*ft_tokenizer(const char *prompt)
 	token_list = NULL;
 	while (prompt[i])
 	{
+		{
+			if (ft_isspace(prompt[i]))
+				i++;
+		}
 		if (prompt[i] == '<' || prompt[i] == '>')
 		{
 			if (prompt[i] == prompt[i + 1])
@@ -228,16 +482,39 @@ t_token	*ft_tokenizer(const char *prompt)
 			}
 			else
 				ft_redir(&token_list, prompt[i]);
+			i++;
 		}
 		else if (prompt[i] == '|')
+		{
 			ft_pipe_token(&token_list);
+			i++;
+		}
 		else if (prompt[i] == '\'' || prompt[i] == '\"')
-			i = ft_loqueseaprimo();
+		{
+			i = ft_quotes_token(&token_list, prompt, i);
+			if (i == -1)
+			{
+				ft_free_token_list(&token_list);
+				return (NULL);
+			}
+		}
 		else if (prompt[i] == '$')
+		{
 			i = ft_var_token(&token_list, prompt, i);
-		else
-			;//caract normales
-		i++;
+			if (i == -1)
+			{
+				ft_free_token_list(&token_list);
+				return (NULL);
+			}
+			i++;
+		}
+		else if (ft_is_word_char(prompt[i]))
+			i = ft_word_token(&token_list, prompt, i);
+		if (i == -1)
+		{
+			ft_free_token_list(&token_list);
+			return (NULL);
+		}
 	}
 	return (token_list);
 }
@@ -247,13 +524,8 @@ int	main(void)
 	t_token	*tokens;
 
 	tokens = NULL;
-	tokens = ft_tokenizer("<<<>>>");
-	/*ft_add_token(&tokens, ft_new_token(T_WORD, ft_strdup("echo")));
-	ft_add_token(&tokens, ft_new_token(T_STRING,
-			ft_strdup("hola mundo")));
-	ft_add_token(&tokens, ft_new_token(T_REDIR_OUT, ft_strdup(">")));
-	ft_add_token(&tokens, ft_new_token(T_WORD,
-			ft_strdup("salida.txt")));*/
+	tokens = ft_tokenizer("$   '' $  $$  $ \ \"$hola\" '$USER'|||<<<>>\'\' \"\" $$$ $$$$ ");
 	ft_print_tokens(tokens);
+	ft_free_token_list(&tokens);
 	return (0);
 }
