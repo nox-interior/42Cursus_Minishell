89de5e92c7f9b9d281c3c092b1b70ab91b042d91
nox <mariam.arroyo@gmail.com>
2025-09-26 15:19:52 +0200

    fds abiertos

--- src/executor/02_builtin_executor.c
+++ src/executor/02_builtin_executor.c
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 18:04:01 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/09/26 15:13:01 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/09/26 15:18:41 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,29 +44,29 @@ static int	ft_execute_builtin_cmd(t_command *cmd, t_shell *shell)
 	return (0);
 }
 
-// int	ft_exec_builtin(t_command *cmd, t_shell *shell)
-// {
-// 	int	saved_stdin;
-// 	int	saved_stdout;
-// 	int	result;
+int	ft_exec_builtin(t_command *cmd, t_shell *shell)
+{
+	int	saved_stdin;
+	int	saved_stdout;
+	int	result;
 
-// 	if (!cmd || !cmd->argv || !cmd->argv[0])
-// 		return (1);
-// 	saved_stdin = dup(STDIN_FILENO);
-// 	saved_stdout = dup(STDOUT_FILENO);
-// 	if (ft_setup_redirection(cmd, shell) == -1)
-// 	{
-// 		close(saved_stdin);
-// 		close(saved_stdout);
-// 		return (1);
-// 	}
-// 	result = ft_execute_builtin_cmd(cmd, shell);
-// 	dup2(saved_stdin, STDIN_FILENO);
-// 	dup2(saved_stdout, STDOUT_FILENO);
-// 	close(saved_stdin);
-// 	close(saved_stdout);
-// 	return (result);
-// }
+	if (!cmd || !cmd->argv || !cmd->argv[0])
+		return (1);
+	saved_stdin = dup(STDIN_FILENO);
+	saved_stdout = dup(STDOUT_FILENO);
+	if (ft_setup_redirection(cmd, shell) == -1)
+	{
+		close(saved_stdin);
+		close(saved_stdout);
+		return (1);
+	}
+	result = ft_execute_builtin_cmd(cmd, shell);
+	dup2(saved_stdin, STDIN_FILENO);
+	dup2(saved_stdout, STDOUT_FILENO);
+	close(saved_stdin);
+	close(saved_stdout);
+	return (result);
+}
 
 // int	ft_exec_builtin(t_command *cmd, t_shell *shell)
 // {
@@ -129,30 +129,30 @@ static int	ft_execute_builtin_cmd(t_command *cmd, t_shell *shell)
 // 	return (status);
 // }
 
-int	ft_exec_builtin(t_command *cmd, t_shell *shell)
-{
-	int	saved_stdin;
-	int	saved_stdout;
-	int	status;
+// int	ft_exec_builtin(t_command *cmd, t_shell *shell)
+// {
+// 	int	saved_stdin;
+// 	int	saved_stdout;
+// 	int	status;
 	
-	saved_stdin = -1;
-	saved_stdout = -1;
-	if (!cmd || !cmd->argv || !cmd->argv[0])
-		return (0);
-	if (cmd->argv[0] && ft_strcmp(cmd->argv[0], "exit") == 0)
-		return (ft_execute_builtin_cmd(cmd, shell));
-	saved_stdin = dup(STDIN_FILENO);
-	saved_stdout = dup(STDOUT_FILENO);
-	if (saved_stdin == -1 || saved_stdout == -1)
-	{
-		if (saved_stdin != -1) close(saved_stdin);
-		if (saved_stdout != -1) close(saved_stdout);
-		return (1);
-	}
-	status = ft_execute_builtin_cmd(cmd, shell);
-	if (dup2(saved_stdin, STDIN_FILENO) == -1) status = 1;
-	if (dup2(saved_stdout, STDOUT_FILENO) == -1) status = 1;
-	close(saved_stdin);
-	close(saved_stdout);
-	return (status);
-}
+// 	saved_stdin = -1;
+// 	saved_stdout = -1;
+// 	if (!cmd || !cmd->argv || !cmd->argv[0])
+// 		return (0);
+// 	if (cmd->argv[0] && ft_strcmp(cmd->argv[0], "exit") == 0)
+// 		return (ft_execute_builtin_cmd(cmd, shell));
+// 	saved_stdin = dup(STDIN_FILENO);
+// 	saved_stdout = dup(STDOUT_FILENO);
+// 	if (saved_stdin == -1 || saved_stdout == -1)
+// 	{
+// 		if (saved_stdin != -1) close(saved_stdin);
+// 		if (saved_stdout != -1) close(saved_stdout);
+// 		return (1);
+// 	}
+// 	status = ft_execute_builtin_cmd(cmd, shell);
+// 	if (dup2(saved_stdin, STDIN_FILENO) == -1) status = 1;
+// 	if (dup2(saved_stdout, STDOUT_FILENO) == -1) status = 1;
+// 	close(saved_stdin);
+// 	close(saved_stdout);
+// 	return (status);
+// }
